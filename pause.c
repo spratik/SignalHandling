26ac4a1ffbff8cc2daeeff1086d1b395bba2feb9
spratik <solanki93pratik@gmail.com>
2017-08-15 15:11:11 +0530

    pause process and waiting for any signal

---
+++ pause.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <signal.h>
+
+void MY_ISR(int num)
+{
+	printf("signal NUM :: %d\n", num);
+	//signal(SIGINT, SIG_DFL);
+
+}
+
+int main()
+{
+	printf("PID : %d\n", getpid());
+	printf("HAI........\n");
+	/// Here changing the default behivour of SIGINT TO MY_ISR so afer this change SIGINT default behivour change
+	signal(SIGINT, MY_ISR);
+	
+	/// Pause will block the process and waiting for any signal onece get signal it will unblock
+	pause();
+	printf("BYE.............\n");
+	while(1);
+	return 0;
+}
